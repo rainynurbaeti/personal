979695606a77d9655dfd084d6267432bca4384c8
rainynurbaeti <rainynurbaetiputri@smkwikrama.sch.id>
2023-12-13 21:26:52 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport"content="width=device-width,initial-scle=1.0">
+    <title>Portofolio</title>
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+     <!-- custom css file link  -->
+     <link rel="stylesheet" href="css/style.css">
+
+</head> 
+<body>
+    
+
+<!-- header section starts -->
+
+<header class="header">
+
+<a href="#home" class="logo">portofolio.</a>
+<nav class="navbar">
+    <a href="#home">home</a>
+    <a href="#about">about</a>
+    <a href="#service">service</a>
+    <a href="#portofolio">portofolio</a>
+    <a href="#contact">contact</a>
+</nav>
+  
+   <div class="follow">
+     <a href="#" class="fab fa-facebook-f"></a>
+     <a href="#" class="fab fa-instagram"></a>
+     <a href="#" class="fab fa-github"></a>
+   </div>
+
+</header>
+
+<!-- header section eds -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+</body>
+</html>
